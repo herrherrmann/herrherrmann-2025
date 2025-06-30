937c899ad28c370cab8c93c1018aa1c0b0852c24
Sebastian Herrmann <sebastian@herrherrmann.net>
2025-06-30 11:18:23 +0200

    Add simple date to blog post layout

--- src/services/date.ts
+++ src/services/date.ts
@@ -1,3 +1,11 @@
 export function formatDate(date: Date) {
   return date.toISOString().slice(0, 10);
 }
+
+export function formatDatePretty(date: Date) {
+  return date.toLocaleDateString("en-UK", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
+}
