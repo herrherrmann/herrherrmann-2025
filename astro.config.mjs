39141ecbb0ffc6ef2f02a3331dc94a7951480e45
Sebastian Herrmann <sebastian@herrherrmann.net>
2026-02-01 22:11:36 +0100

    Add automatic last modified date to /uses page (#2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
 // @ts-check
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import remarkModifiedTime from "./remark-plugins/remark-modified-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://herrherrmann.net",
   trailingSlash: "always",
 
+  markdown: {
+    remarkPlugins: [remarkModifiedTime],
+  },
+
   image: {
     responsiveStyles: true,
     layout: "constrained",
   },
 
-  integrations: [sitemap()],
+  integrations: [mdx(), sitemap()],
 });
